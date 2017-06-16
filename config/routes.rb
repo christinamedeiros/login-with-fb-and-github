d0474e300554d3f1cf53244d1a909a037bb111e6
Christopher <cvald016@fiu.edu>
2017-06-16 14:34:52 -0400

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   get 'logout' => 'sessions#destroy'
 
   match 'auth/facebook/callback', to: 'sessions#fbcreate', via: [:get, :post]
-match 'auth/failure', to: redirect('/'), via: [:get, :post]
-match 'signout', to: 'sessions#fbdestroy', as: 'signout', via: [:get, :post]
+  match 'auth/failure', to: redirect('/'), via: [:get, :post]
+  match 'signout', to: 'sessions#fbdestroy', as: 'signout', via: [:get, :post]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
